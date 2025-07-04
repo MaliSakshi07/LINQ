e72e7b635a326df09ed9642c0eb1b2bbd2ca02fc
MaliSakshi07 <sakshimali467@gmail.com>
2025-07-04 21:13:54 +0530

    LINQ

---
+++ 03_OrderBy/02 OrderBy/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_OrderBy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] age = { 10, 11, 22, 34, 65, 43, 6, 7, 75, 67, 45 };
+            var no = from value in age where value > 18 orderby value descending select value;
+
+            foreach (int item in no)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("\nThese Candidates are eligible for vote...");
+            Console.ReadKey();
+        }
+    }
+}
