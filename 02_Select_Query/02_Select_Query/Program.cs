9e671175b6e7aea240b253063f295c5b1d34b738
MaliSakshi07 <sakshimali467@gmail.com>
2025-07-04 21:13:04 +0530

    LINQ

---
+++ 02_Select_Query/02_Select_Query/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Select_Query
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "sakshi", "akanksha", "sanskruti", "maya", "snehal" };
+            var value = from name in names where name.StartsWith("s") select name;
+
+            foreach(string item in value)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
